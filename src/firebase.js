f14ca5994cdafdb704283c235649d43453019601
VehanRajintha <IT23646360@my.sliit.lk>
2024-11-30 13:07:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZBy7zFUAetKKf2hzlaMKp4FXNGd8H1yg",
-  authDomain: "chatx-c93e0.firebaseapp.com",
-  projectId: "chatx-c93e0",
-  storageBucket: "chatx-c93e0.firebasestorage.app",
-  messagingSenderId: "632475885672",
-  appId: "1:632475885672:web:184b0b5d4937bc432a9bdd",
-  measurementId: "G-84R7PJ32VB"
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
 const app = initializeApp(firebaseConfig);
